94d1e2df5c1be0953b94e5d5ad030a2d779fafd8
herrstrietzel <der@herrstrietzel.de>
2025-05-12 01:02:32 +0200

    update npmignore

--- demo/app.js
+++ demo/app.js
@@ -1,27 +1,26 @@
-        let svgEl = document.getElementById("svg");
-        let svgStr = new XMLSerializer().serializeToString(svgEl);
-        //console.log(svgStr)
-        let svgInp = svgEl;
+let svgEl = document.getElementById("svg");
+let svgStr = new XMLSerializer().serializeToString(svgEl);
+//console.log(svgStr)
+let svgInp = svgEl;
 
 
-        (async () => {
-            let options = {
-                addGlobalStyles: true,
-                format: 'webp',
-                scaleImages: false,
-                quality: 0.8,
-                addToolbar: true
-            }
+(async () => {
+    let options = {
+        addGlobalStyles: true,
+        format: 'webp',
+        scaleImages: false,
+        quality: 0.8,
+        addToolbar: true
+    }
 
 
-            /*
-            let savgEls = new savg(svgEl);
-            console.log('savg', savg);
-            */
+    /*
+    let savgEls = new savg(svgEl);
+    console.log('savg', savg);
+    */
 
-            // self contained img
-            let savg2 = new savg(imgSelfContained); 
+    // self contained img
+    let savg2 = new savg(imgSelfContained);
+})();
 
 
-
-        })();
